f320721c859ca7a21c5c189b15468fb8e778e1ad
AbuZhuma <abdirahman.zhumagulov@gmail.com>
2025-02-13 19:36:24 +0600

    test

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,15 @@ const express = require('express');
 const register = require('../controllers/users/reg');
 const login = require('../controllers/users/login');
 const authenticateJWT = require('../middlewares/checkToken');
+const User = require('../models/user');
 const router = express.Router();
 
 router.post("/reg", register)
 router.post("/login", login)
+router.get("/", async(req, res) => {
+      let allUsers = await User.find({})
+      res.status(200).json(allUsers) 
+})
 router.get("/isexist", authenticateJWT, (req, res) => {
       res.status(200).json({message: "user exist"})
 })
