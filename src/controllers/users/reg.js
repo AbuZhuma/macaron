11be64814152f76c40bbfe9347a382d8448b16ea
AbuZhuma <abdirahman.zhumagulov@gmail.com>
2025-02-13 20:11:49 +0600

    test

--- src/controllers/users/reg.js
+++ src/controllers/users/reg.js
@@ -1,12 +1,12 @@
 const checkUser = require("../../helpers/checkUser")
 const generateRandomID = require("../../helpers/genIdH")
 const User = require("../../models/user")
+const jwt = require("jsonwebtoken");
 
 const register = async(req, res) => {
       try {
             const body = req.body
-            console.log(body);
-            
+            const SECRET_KEY = process.env.SECRET_KEY;
             if(!body || !body.username || !body.password || !body.city || !body.email || !body.company || !body.phone){
                   res.status(501).send("Check you`r fields!")
                   return
@@ -28,14 +28,22 @@ const register = async(req, res) => {
                   createdat: new Date()
             }
             const newUser = new User(options)
-            newUser.save()
+            await newUser.save()
+            const token = jwt.sign({ username: body.usersname,userid:id }, SECRET_KEY, { expiresIn: '20h' });
+            const answ = {
+              message: "User is authenticated!",
+              token: token,
+              userid: id,
+              username: body.username
+            };  
+
             if(newUser){
-               return res.status(200).json({
-                  message: "User registered!"
-               })  
+               return res.status(200).json(answ)  
             }
             res.status(400).send("Please try again later!")
       } catch (error) {
+            console.log(error);
+            
             res.status(501).send(error)
       }
 }
